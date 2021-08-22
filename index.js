a0fae35dbc4a3d156ed1279a88434da9be4af5fe
Frank Mckenna <fmckenna56330@gmail.com>
2021-08-22 20:58:52 +0100

    chore: adding files for heroku setup

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-let skill = document.getElementById('skill');
-let bars = document.querySelectorAll("[data-bar-id]");
-let i = 0;
+document.addEventListener('DOMContentLoaded', () => {
+  let skill = document.getElementById('skill');
+  const bars = document.querySelectorAll("[data-bar-id]");
+  let i = 0;
 
-const progressBars = () => {
-  if (i == 0) {
-    i = 1;
-    bars.forEach(bar => {
-      var width = 10
-      const elem = bar
-      const progress = bar.dataset.percent;
-      let interval = setInterval(frame, 20);
-      function frame() {
-        if (width >= progress) {
-          clearInterval(interval);
-          i = 0;
-        } else {
-          width++;
-          elem.style.width = width + "%";
-          elem.innerHTML = width + "%";
+  const progressBars = () => {
+    if (i == 0) {
+      i = 1;
+      bars.forEach(bar => {
+        var width = 10
+        const elem = bar
+        const progress = bar.dataset.percent;
+        let interval = setInterval(frame, 20);
+        function frame() {
+          if (width >= progress) {
+            clearInterval(interval);
+            i = 0;
+          } else {
+            width++;
+            elem.style.width = width + "%";
+            elem.innerHTML = width + "%";
+          }
         }
-      }
-    })
+      })
+    }
+    skill.removeEventListener('mouseover', progressBars, false);
   }
-  skill.removeEventListener('mouseover', progressBars, false);
-}
 
-skill.addEventListener("mouseover", progressBars, false)
+  skill.addEventListener("mouseover", progressBars, false)
+});
